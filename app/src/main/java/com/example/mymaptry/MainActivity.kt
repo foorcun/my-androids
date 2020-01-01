bd3a9141f884858aa9345d8cd969578186d75b03
msnifoorcun@hotmail.com <Muse2321>
2020-01-01 13:45:46 +0300

    haritayı açtık valla hiüüü

---
+++ app/src/main/java/com/example/mymaptry/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mymaptry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var  mapFragment : SupportMapFragment
+    lateinit var googleMap : GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        mapFragment.getMapAsync(OnMapReadyCallback {
+
+        })
+
+
+
+
+    }
+}
