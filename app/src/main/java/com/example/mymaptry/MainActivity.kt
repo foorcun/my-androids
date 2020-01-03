a68271d6c12038ca7d8b8940ed5fc4c0d904beac
msnifoorcun@hotmail.com <Muse2321>
2020-01-03 13:01:23 +0300

    location mark ve current location

--- app/src/main/java/com/example/mymaptry/MainActivity.kt
+++ app/src/main/java/com/example/mymaptry/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.mymaptry
 
+import android.content.Context
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var  mapFragment : SupportMapFragment
-    lateinit var googleMap : GoogleMap
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +23,19 @@ class MainActivity : AppCompatActivity() {
         mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
 
         mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+            googleMap.isMyLocationEnabled = true //current location
 
+
+            //map marker eklemek
+            var location1 = LatLng(40.343144, 26.685169)
+            googleMap.addMarker(MarkerOptions().position(location1).title("rastgele"))
+
+            // zooming
+            googleMap.animateCamera((CameraUpdateFactory.newLatLngZoom(location1, 1f)))
         })
 
 
-
-
     }
+
 }
